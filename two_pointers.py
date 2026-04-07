79a8377eacd62de42525c6733567b989e0593cd4
niranjan <niranjan740@gmail.com>
2026-04-07 18:17:39 +0530

    Add maxArea function

--- two_pointers.py
+++ two_pointers.py
@@ -78,3 +78,24 @@ class Solution:
                 if val > 0:
                     right = right - 1
         return output
+
+# ================================================
+# Date: 8 Apr 2026
+# Problem: Container With Most Water
+# Pattern: Two Pointers
+# Solved alone? No — guided
+# ================================================
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left = 0
+        right = len(heights) - 1
+        max_area = 0
+        while left < right:
+            area = (right- left) * min(heights[right], heights[left])
+            max_area = max(max_area, area)
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
